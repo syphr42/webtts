ecb1535db0b2ade2d5edbafaef8cea88a738e48b
Gregory Moyer <moyerg@syphr.com>
2016-10-14 21:04:33 -0400

    first attempt at travis ci integration

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+TEST_HOST="${TEST_HOST:?The environment variable 'TEST_HOST' must be set and non-empty}"
+
+curl -v -s -I -w "%{http_code}" -X GET "http://$TEST_HOST/tts.php?lng=en-US&msg=Hello World" | grep -q 200
