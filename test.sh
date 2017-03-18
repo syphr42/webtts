21fbbd3f8d4b05132df4687c8a18f2ca272b345c
Gregory Moyer <moyerg@syphr.com>
2017-03-18 19:12:39 -0400

    Remove spaces from test URL

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 TEST_HOST="${TEST_HOST:?The environment variable 'TEST_HOST' must be set and non-empty}"
 
-curl -v -s -I -w "%{http_code}" -X GET "http://$TEST_HOST/tts.php?lng=en-US&msg=Hello World" | grep -q 200
+curl -v -s -I -w "%{http_code}" -X GET "http://$TEST_HOST/tts.php?lng=en-US&msg=Hello+World" | grep -q 200
